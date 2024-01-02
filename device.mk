1cc92e6541256d081268fc8beb5eea8ab9b7f23f
Asriadi Rahim <asriadirahim03@gmail.com>
2024-01-02 20:05:22 +0000

    taimen: Define fstab.hardware * Since walleye and taimen require repartitioning so the definition of the system_ext is also different, we need different fstab adjustments, on the other hand this is necessary because we are no longer mounting from the kernel side. * Switch to eucalyptus kernel
    Signed-off-by: Asriadi Rahim <asriadirahim03@gmail.com>

--- device.mk
+++ device.mk
@@ -70,6 +70,11 @@ PRODUCT_COPY_FILES += \
     device/google/taimen/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
     device/google/taimen/thermal_info_config_evt.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_evt.json
 
+# Fstab Hardware
+PRODUCT_COPY_FILES += \
+    device/google/taimen/fstab.hardware:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE) \
+    device/google/taimen/fstab.hardware:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.$(PRODUCT_HARDWARE)
+
 # Audio
 PRODUCT_COPY_FILES += \
     device/google/taimen/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil_taimen.xml \
