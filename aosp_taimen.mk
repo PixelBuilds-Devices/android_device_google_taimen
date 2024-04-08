50714302c25eeee9c1050da9b345a742df2fa66b
PixelBoot <bootingpixels@gmail.com>
2024-04-08 13:07:52 +0200

    taimen: nuke stock fingerprint

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -43,7 +43,4 @@ PRODUCT_DEVICE := taimen
 PRODUCT_MODEL := Pixel 2 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
-
-BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
+    PRODUCT_NAME=taimen
